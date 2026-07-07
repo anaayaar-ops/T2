34ed7001bd944795cd4348fc32a9dcc2ffd27c45
anaayaar-ops <ana.ayaar@gmail.com>
2026-07-08 00:15:24 +0300

    Update console logs to read Channel.js content

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import 'dotenv/config';
 import fs from 'fs';
 
-console.log("=== محتوى helper/event/Event.js ===\n");
-console.log(fs.readFileSync('./node_modules/wolf.js/src/helper/event/Event.js', 'utf8'));
+console.log("=== محتوى helper/event/Channel.js ===\n");
+console.log(fs.readFileSync('./node_modules/wolf.js/src/helper/event/Channel.js', 'utf8'));
