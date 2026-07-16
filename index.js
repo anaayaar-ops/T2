fe8e666ee919d00032e5802b082cac9ffcd984ac
anaayaar-ops <ana.ayaar@gmail.com>
2026-07-16 05:14:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-import 'dotenv/config';
+import { WOLF } from 'wolf.js';
 import fs from 'fs';
-import sharp from 'sharp';
-import wolfjs from 'wolf.js';
-const { WOLF } = wolfjs;
+import imageSize from 'image-size';
 
 const client = new WOLF();
+
+client.on('ready', () => {
+  console.log('Bot is ready!');
+
+  // اطبع إعدادات الأفاتار الخاصة بالقنوات (الأنواع المسموحة + الحجم الأقصى)
+  console.log(client._frameworkConfig.get('multimedia.avatar.channel'));
+});
+
+client.login();
